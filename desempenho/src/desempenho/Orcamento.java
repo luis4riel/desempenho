721edc23891575dbf4576c407038eef29deed035
luis4riel <zluisariel@gmail.com>
2018-10-24 15:31:35 -0200

    Adicionando classe orçamento

---
+++ desempenho/src/desempenho/Orcamento.java
@@ -0,0 +1,18 @@
+package desempenho;
+
+/**
+ *
+ * @author Luis Ariel Zanella
+ */
+public class Orcamento {
+
+    private double valor;
+
+    public Orcamento(double valor) {
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+}
