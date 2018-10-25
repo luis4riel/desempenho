746d459ac1040a70dbf580044406fb401d91da50
luis4riel <zluisariel@gmail.com>
2018-10-25 20:13:28 -0200

    Adicionando primeiro caso de teste

---
+++ desempenho/test/desempenho/ImpostoTest.java
@@ -0,0 +1,46 @@
+package desempenho;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Luis Ariel Zanella
+ */
+public class ImpostoTest {
+
+    public ImpostoTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of calcula method, of class Imposto.
+     */
+    @Test
+    public void testCalcula() {
+        Orcamento orcamento = new Orcamento(100.0);
+        Imposto imposto = new Imposto();
+        // valor esperado, valor obtido
+        assertEquals(10.0, imposto.calcula(orcamento), Double.MIN_VALUE);
+    }
+
+}
